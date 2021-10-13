d1109ba544316637e01e4e71b44329eeaa55d4dd
uj <julien.abadji@inria.fr>
2021-10-13 16:24:27 +0200

    add other init methods

--- src/blocklist.rs
+++ src/blocklist.rs
@@ -31,6 +31,16 @@ pub struct Blocklist<'a> {
 }
 
 impl<'a> Blocklist<'a> {
+    /// Create a new Blocklist of provided kind.
+    ///
+    pub fn new(kind: &'a str, domains: HashSet<String>, urls: HashSet<String>) -> Self {
+        Self {
+            kind,
+            domains,
+            urls,
+        }
+    }
+
     /// create a blocklist from specified kind and folder.
     ///
     /// It will look for  `path/of/the/folder/kind`.
@@ -69,16 +79,22 @@ impl<'a> Blocklist<'a> {
         })
     }
 
-    /// Get a reference to the blocklist's domains.
-    pub fn domains(&self) -> &HashSet<String> {
-        &self.domains
+    /// Create [Blocklist] with default folder (`./ut1-blacklists/blacklists/`) and default kind (`adult`).
+    pub fn with_default_folder() -> Result<Self, Error> {
+        let default_folder = PathBuf::from("./ut1-blacklists/blacklists/");
+        Self::with_folder("adult", &default_folder)
     }
+
+    /// Get a reference to the blocklist's domains.
+    // pub fn domains(&self) -> &HashSet<String> {
+    //     &self.domains
+    // }
 
     /// returns `true` if domain of the provided url is in the domains list,
     /// `false` if not, or if there's no domain in the url.
     ///
     pub fn detect_domain(&self, url: &Url) -> bool {
-        if let Some(domain) = url.domain() {
+        if let Some(domain) = url.host_str() {
             self.domains.contains(domain)
         } else {
             false
@@ -96,23 +112,34 @@ impl<'a> Blocklist<'a> {
     }
 
     /// Get a reference to the blocklist's kind.
-    pub fn kind(&self) -> &&'a str {
+    pub fn kind(&self) -> &'a str {
         &self.kind
     }
-}
 
-impl<'a> Default for Blocklist<'a> {
-    /// Looks by default for "./ut1-blacklists/blacklists", getting the "adult" one.
-    fn default() -> Self {
-        let default_folder = PathBuf::from("./ut1-blacklists/blacklists/");
-        Self::with_folder("adult", &default_folder)
-            .unwrap_or_else(|_| panic!("{:?} not found", default_folder))
-    }
+    // /// Get a mutable reference to the blocklist's domains.
+    // pub fn domains_mut(&mut self) -> &mut HashSet<String> {
+    //     &mut self.domains
+    // }
+
+    // /// Get a mutable reference to the blocklist's urls.
+    // pub fn urls_mut(&mut self) -> &mut HashSet<String> {
+    //     &mut self.urls
+    // }
+
+    // /// Set the blocklist's domains.
+    // pub fn set_domains(&mut self, domains: HashSet<String>) {
+    //     self.domains = domains;
+    // }
+
+    // /// Set the blocklist's urls.
+    // pub fn set_urls(&mut self, urls: HashSet<String>) {
+    //     self.urls = urls;
+    // }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{error::Error, fs::File, io::Write, str::FromStr};
+    use std::{collections::HashSet, error::Error, fs::File, io::Write, str::FromStr};
 
     use url::Url;
 
@@ -137,11 +164,37 @@ mod tests {
     }
 
     #[test]
+    fn test_new() {
+        let domains = vec!["foo.bar".to_string()].into_iter();
+        let bl = Blocklist::new("test", domains.collect(), HashSet::new());
+
+        let is_detected = Url::from_str("https://foo.bar").unwrap();
+        let is_not_detected = Url::from_str("https://baz.quux").unwrap();
+
+        assert!(bl.detect_domain(&is_detected));
+        assert!(!bl.detect_domain(&is_not_detected));
+    }
+    #[test]
     fn domain_contains() {
         let url = Url::from_str("https://foo.bar").unwrap();
         let bl = get_test_blocklist().unwrap();
 
         assert!(bl.detect_domain(&url));
+    }
+
+    #[test]
+    fn domain_is_address() {
+        // ensure that addresses are properly retained
+        let url = Url::from_str("https://127.0.0.1").unwrap();
+        let bl = get_test_blocklist().unwrap();
+        assert!(!bl.detect_domain(&url));
+    }
+
+    #[test]
+    fn domain_no_domain() {
+        let url = Url::from_str("mailto::foo@bar.baz").unwrap();
+        let bl = get_test_blocklist().unwrap();
+        assert!(!bl.detect_domain(&url));
     }
 
     #[test]
